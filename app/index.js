1e280db9f569b5f0ba1a023cdc94192e2bf1426f
Jesús Moreira <jesucristhorr@protonmail.com>
2021-02-25 09:37:23 -0500

    Add new change

--- app/index.js
+++ app/index.js
@@ -12,8 +12,13 @@ app.get("/", (req, res) => {
 
 app.get("/api", (req, res) => {
   res.json({
-    message: "Se supone que soy una api, ¡hola!",
+    message: "AA",
   });
 });
 
+app.get("/prueba", (req, res) => {
+  res.json({
+    message: "Mensaje AHC",
+  });
+});
 app.listen(3000, () => console.log("Servidor inicializado."));
