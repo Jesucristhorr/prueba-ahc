1211b5ca09f96f28dac3f09699d843b24fb1e9ee
Jesús Moreira <jesucristhorr@protonmail.com>
2021-02-25 01:53:40 -0500

    Add new api endpoint

--- app/index.js
+++ app/index.js
@@ -10,4 +10,10 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Se supone que soy una api, ¡hola!",
+  });
+});
+
 app.listen(3000, () => console.log("Servidor inicializado."));
